f8e92a9bb7bd4e1e206f9cc1a332775c8048d4a7
jperezdelafuente <jesusperez82@gmail.com>
2022-05-28 13:05:21 +0200

    fixing with import

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.model;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwnerTest {
