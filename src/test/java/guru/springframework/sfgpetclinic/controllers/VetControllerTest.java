f802380bd624ae7b890f1a2c67c1369ba8e5e6aa
jperezdelafuente <jesusperez82@gmail.com>
2022-05-28 14:40:55 +0200

    adding refactor with test interfaces and @Tag

--- src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.ControllerTests;
 import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.fauxspring.ModelMapImpl;
 import guru.springframework.sfgpetclinic.model.Vet;
@@ -18,8 +19,7 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@Tag("controllers")
-class VetControllerTest {
+class VetControllerTest implements ControllerTests {
 
     SpecialtyService specialtyService;
     VetService vetService;
